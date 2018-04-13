7c9c697d2c09d8abbb729e7f97ce129d9ebabcbd
Joey Lee <slarinz@gmail.com>
2018-04-13 21:30:11 +1000

    created empty classes

---
+++ CoffeeShopFinder/CoffeeShopFinder/System/Network/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  CoffeeShopFinder
+//
+//  Created by Joey Lee on 13/4/18.
+//  Copyright © 2018 Joey Lee. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager: NSObject {
+
+}
---
+++ CoffeeShopFinder/CoffeeShopFinder/UI/Style/Font.swift
@@ -0,0 +1,9 @@
+//
+//  Font.swift
+//  CoffeeShopFinder
+//
+//  Created by Joey Lee on 13/4/18.
+//  Copyright © 2018 Joey Lee. All rights reserved.
+//
+
+import Foundation
