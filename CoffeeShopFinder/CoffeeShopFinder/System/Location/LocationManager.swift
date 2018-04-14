e570dc4d7802966ec89f2efcf13de81c75dab94f
Joey Lee <slarinz@gmail.com>
2018-04-14 17:37:07 +1000

    Write code in SplashViewController, LocationManager, and related files
    App will check if it is allowed to access user's location on `SplashViewController`.
    If it is not determined, then system will ask to user to allow it.
    After granted, `CoffeeShopListViewController` will be shown.

--- CoffeeShopFinder/CoffeeShopFinder/System/Location/LocationManager.swift
+++ CoffeeShopFinder/CoffeeShopFinder/System/Location/LocationManager.swift
@@ -6,8 +6,100 @@
 //  Copyright © 2018 Joey Lee. All rights reserved.
 //
 
-import UIKit
+import CoreLocation
 
-class LocationManager: NSObject {
+class LocationManager: NSObject, CLLocationManagerDelegate {
 
+    // Singleton
+    static let shared = LocationManager()
+    
+    // defines
+    typealias AuthorizationHandler = (CLAuthorizationStatus) -> Void
+    typealias LocationHandler = (CLLocation?, Error?) -> Void
+    static var NotificationAuthorizationChange = NSNotification.Name(rawValue: "NotificationAuthorizationChange")
+    
+    // variables
+    var locationManager = CLLocationManager()
+    var authorizationHandler: AuthorizationHandler? = nil
+    var locationHandler: LocationHandler? = nil
+    
+    override init() {
+        super.init()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+    }
+    
+    // MARK: - public functions
+    public func requestWhenInUseAuthorization(_ handler: @escaping AuthorizationHandler) {
+        let status = authorizationStatus()
+        if status == .notDetermined {
+            authorizationHandler = handler
+            locationManager.requestWhenInUseAuthorization()
+        }
+        else {
+            handler(status)
+        }
+    }
+    public func authorizationStatus() -> CLAuthorizationStatus {
+        return CLLocationManager.authorizationStatus()
+    }
+    public func startUpdatingLocation(_ handler: @escaping LocationHandler) {
+        locationHandler = handler
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        //TODO: clean up this code after testing
+        switch status {
+        case .notDetermined:
+            print("location autorization: not determined")
+        case .restricted:
+            print("location autorization: restricted")
+        case .denied:
+            print("location autorization: denined")
+        case .authorizedAlways:
+            print("location autorization: authoized always")
+        case .authorizedWhenInUse:
+            print("location autorization: authoized when is use")
+        }
+        
+        if status != .notDetermined {
+            if let handler = authorizationHandler {
+                handler(status)
+                authorizationHandler = nil
+            }
+        }
+        
+        // post notification
+        let granted = (status == .authorizedWhenInUse || status == .authorizedAlways)
+        NotificationCenter.default.post(name: LocationManager.NotificationAuthorizationChange,
+                                        object: self,
+                                        userInfo: ["granted":granted])
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let eventDate = location.timestamp
+            let howRecent = eventDate.timeIntervalSinceNow
+            if (fabs(howRecent) < 15.0) {
+                manager.stopUpdatingLocation()
+                
+                if let handler = locationHandler {
+                    handler(location, nil)
+                    locationHandler = nil
+                }
+            }
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        manager.stopUpdatingLocation()
+        
+        if let handler = locationHandler {
+            handler(nil, error)
+            locationHandler = nil
+        }
+    }
 }
--- CoffeeShopFinder/CoffeeShopFinder/UI/Screens/Splash/SplashViewController.swift
+++ CoffeeShopFinder/CoffeeShopFinder/UI/Screens/Splash/SplashViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SplashViewController: BaseViewController {
 
@@ -17,11 +18,51 @@ class SplashViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.navigationController?.pushViewController(CoffeeShopListViewController(), animated: true)
-        }
+        requestWhenInUseAuthorization()
     }
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    // MARK: - private functions
+    private func checkAuthoizationStatus(_ status: CLAuthorizationStatus) {
+        
+        switch status {
+        case .notDetermined:
+            print("not determined")
+            requestWhenInUseAuthorization()
+        case .restricted:
+            print("restricted")
+            UIAlertController.alert(text: "Unable to access your location. Please allow your location to continue at Settings app.") {
+                self.requestWhenInUseAuthorization()
+            }
+        case .denied:
+            print("denined")
+            UIAlertController.alert(text: "Unable to access your location. Please allow your location to continue at Settings app.") {
+                self.requestWhenInUseAuthorization()
+            }
+        case .authorizedAlways:
+            print("authoized always")
+            moveToLocationList()
+        case .authorizedWhenInUse:
+            print("authoized when is use")
+            moveToLocationList()
+        }
+    }
+    private func animateLogo(completion: @escaping (() -> Void)) {
+        //TODO: implement an animation later...
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            completion()
+        }
+    }
+    private func moveToLocationList() {
+        animateLogo {
+            self.navigationController?.pushViewController(CoffeeShopListViewController(), animated: true)
+        }
+    }
+    private func requestWhenInUseAuthorization() {
+        LocationManager.shared.requestWhenInUseAuthorization({ (status) in
+            self.checkAuthoizationStatus(status)
+        })
     }
 }
